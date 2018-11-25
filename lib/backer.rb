f77542368ea925822e4eeb18007a290a604058bb
grant smith <grantsmith1256@gmail.com>
2018-11-25 02:17:36 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,12 +4,12 @@ class Backer
   
   attr_accessor :name, :backed_projects
   
-  @@all_backers = []
   
   def initialize(name)
     @name = name
     @backed_projects = []
-    @@all_backers << self
+    @backed_projects - []
+    
   end
   
   def back_project(project)
@@ -17,7 +17,7 @@ class Backer
   end
   
   def backers
-    @@all_backers
+    @all_backers << self
   end
   
   
